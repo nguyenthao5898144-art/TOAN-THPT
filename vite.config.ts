f7907efcfe7d8eaa5d8fd378697b04c30bf4a376
Nguyễn Quốc Tâm <nguyenthao5898144@gmail.com>
2026-08-31 10:51:28 +0700

    Update external dependencies in vite.config.ts
    Added multiple external dependencies to Rollup options.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['fsevents']
+      external: [
+        'fsevents', 'path', 
+        './1.tsx', './12.tsx', './13.tsx', './14.tsx', './15.tsx',
+        './16.tsx', './17.tsx', './18.tsx', './19.tsx', './20.tsx',
+        './21.tsx', './22.tsx', './23.tsx', './24.tsx', './25.tsx'
+      ]
     }
   }
 });
