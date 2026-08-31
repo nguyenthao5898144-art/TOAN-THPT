38d99fae95d8bcb96f336e6665293dc7c8832c2c
Nguyễn Quốc Tâm <nguyenthao5898144@gmail.com>
2026-08-31 12:58:00 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 // Kích hoạt điểm neo root để hiển thị toàn bộ giao diện React
 createRoot(document.getElementById('root')!).render(
