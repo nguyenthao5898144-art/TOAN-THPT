10916402404bc1543115cc8b0116346a33b1a7d5
Nguyễn Quốc Tâm <nguyenthao5898144@gmail.com>
2026-09-01 08:37:48 +0700

    Update 13.tsx

--- src/13.tsx
+++ src/13.tsx
@@ -1,4 +1,4 @@
-import { TestConfig, GeneratedTest, Question } from '../types';
+import { TestConfig, GeneratedTest, Question } from './types';
 import { createDefaultTest, mutateQuestionToMakeUnique, deduplicateAllQuestions, calculateMatrixAndSummary } from './testGenerator';
 
 /**
