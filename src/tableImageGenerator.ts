eb6784a6d1207779f060c2026a6a3dce44f5f0db
Nguyễn Quốc Tâm <nguyenthao5898144@gmail.com>
2026-09-04 19:38:03 +0700

    Delete src directory

--- src/tableImageGenerator.ts
+++
@@ -1,7 +0,0 @@
-import { GeneratedTest } from './types';
-
-export async function generateTableImage(test: GeneratedTest): Promise<string> {
-  console.log('Generating table image for test:', test.id);
-  // Sử dụng cấu trúc thuần túy, loại bỏ hoàn toàn các thẻ component viết hoa gây lỗi
-  return '';
-}
